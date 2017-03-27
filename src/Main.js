609a8c8badb6b51ec2ebb8b54abe5d74441a1bbe
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-03-27 12:42:15 +0200

    working search

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,8 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import IconButton from 'material-ui/IconButton';
 import ActionSearch from 'material-ui/svg-icons/action/search';
+import CircularProgress from 'material-ui/CircularProgress';
+
 import GoogleFonts from './google-fonts.json';
 
 let space = {margin: 10};
@@ -24,22 +26,113 @@ let imgStyle = {
   margin: 10
 };
 
+function str(o) {
+  try {
+    return JSON.stringify(o, null, 2);
+  } catch(e) {
+    return String(o);
+  }
+}
+
+class SearchCQL extends React.Component {
+  constructor(props, context) {
+    super(props);
+    this.state = {
+      query: '',
+      searching: false,
+      error: '',
+      results: []
+    };
+  }
+  set(k, v) { 
+    this.state[k] = v;
+    this.setState(this.state);
+  }
+  render() {
+    let dbc = window.dbcOpenPlatform;
+    return <Paper style={{
+      padding: '0px 0px 10px 30px'
+    }}>
+    <TextField 
+      style={{width: '80%'}}
+      value={this.state.query}
+      onChange={(_, val)=>{
+        this.set('results', []);
+        this.set('error', '');
+        this.set('query', val);
+      }}
+      floatingLabelText="CQL Søgning"/>
+
+    <IconButton onClick={async ()=>{
+      this.set('searching', true);
+      this.set('error', '');
+      try {
+        if(!dbc.connected()) {
+          await dbc.connect(
+            // TODO: use actual client-id, and local library branch
+            "a36227da-e477-491e-b4a2-ccd9df365cf9", 
+            "YfO7hc8OJ+vUGh9GhMZhJw06cyHxNi48fwWnVLJGPrPHvkZaYYj0cboM");
+        }
+        this.set('results',
+          await window.dbcOpenPlatform.search({
+            q: this.state.query, limit: 20,
+            fields: ["title", "creator", "coverUrlThumbnail"]}));
+      } catch(e) { this.set('error', str(e)) }
+      this.set('searching', false);
+    }}>
+    {this.state.searching 
+      ? <CircularProgress size={32} />
+      : <ActionSearch/> }
+    </IconButton>
+
+    {this.state.error && 
+        <div style={{
+          display: 'inline-block',
+          textAlign: 'left'
+        }}>
+        <h3>Error</h3>
+        <pre>{this.state.error}</pre>
+      </div>
+    }
+
+    <div>
+      {this.state.results.map(o => 
+        <div style={{
+          display: 'inline-block',
+          whiteSpace: 'nowrap',
+          textAlign: 'left',
+          height: 42,
+          margin: 1,
+          width: 240,
+          overflow: 'hidden'
+        }}>
+        <img 
+          src={o.coverUrlThumbnail}
+          style={{ 
+            height: 40,
+            width: 30,
+            border: '1px solid black',
+            float: 'left',
+            marginRight: 4
+          }}/>
+        <div>{o.title}</div>
+        <div style={{fontStyle: 'italic'}}>{o.creator}</div>
+      </div>
+      )}
+    </div>
+
+  </Paper>
+  }
+}
+
 export default class Main extends React.Component {
   constructor(props, context) {
     super(props);
     this.state = {ypos:0.8};
-    this.theme = context.muiTheme;
   }
   render() {
-    console.log(this.theme);
     return <div>
-      <Paper style={{textAlign: 'center'}}>
-        <TextField style={{width: '80%'}}
-          floatingLabelText="CQL Søgning"/>
-        <IconButton>
-          <ActionSearch/>
-        </IconButton>
-      </Paper>
+      <SearchCQL/>
       <FloatingActionButton mini={true}>
         <ContentAdd />
       </FloatingActionButton>
@@ -63,7 +156,7 @@ export default class Main extends React.Component {
             verticalAlign: 'bottom'
           }}>
         </Paper>
-      <br/>
+        <br/>
         <TextField 
           floatingLabelText="Y-position"
           type="number" 
@@ -94,14 +187,14 @@ export default class Main extends React.Component {
           style={Object.assign({
             display: 'inline-block',
             width: 200}, space)}
-          labelPosition="right"
-          label="Overskriv"
-          thumbSwitchedStyle={{backgroundColor: '#f00'}}
-          trackSwitchedStyle={{backgroundColor: '#faa'}}
-          labelStyle={{color: '#000'}}
-        />
-        <br/>
-      </Paper>
-    </div>;
+            labelPosition="right"
+            label="Overskriv"
+            thumbSwitchedStyle={{backgroundColor: '#f00'}}
+            trackSwitchedStyle={{backgroundColor: '#faa'}}
+            labelStyle={{color: '#000'}}
+          />
+          <br/>
+        </Paper>
+      </div>;
   }
-  }
+}
