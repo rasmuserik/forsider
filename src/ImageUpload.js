f340b1ee0166ef19f5617a99d1cef5492b48761c
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-03-27 14:52:03 +0200

    load images

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -3,27 +3,73 @@ import Paper from 'material-ui/Paper';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
-let imgStyle = {
-  display: 'inline-block',
-  width: 70,
-  height: 100,
-  verticalAlign: 'middle',
-  margin: 10
-};
 
-export default class Main extends React.Component { 
+function randomId() {
+  return Math.random().toString(36).slice(2,12);
+}
+
+let file2url = (f) => new Promise((resolve) => {
+  let reader = new FileReader();
+  reader.addEventListener('load', () => resolve(reader.result));
+  reader.readAsDataURL(f);
+});
+
+export default class ImageUpload extends React.Component { 
   constructor(props, context) {
     super(props);
+    this.inputId = randomId();
+    this.state = {images: []}
+  }
+  async addImages(imgs) {
+    for(var i = 0; i < imgs.length; ++i) {
+      let img = imgs[i];
+      let url =  await file2url(img);
+      this.setState(o => o.images.push({
+        id: randomId(),
+        name: img.name,
+        url
+      }));
+    }
+    console.log('addImages', imgs);
   }
   render() {
     return <div>
-      <FloatingActionButton mini={true}>
+      <input 
+        type="file"
+        accept="image/*"
+        multiple={true}
+        id={this.inputId} 
+        onChange={o => this.addImages(o.target.files)}
+        style={{display: 'none'}} />
+
+      <FloatingActionButton 
+        onClick={()=>{
+          let elem = document.getElementById(this.inputId);
+          elem.click();
+        }}
+        mini={true}>
         <ContentAdd />
       </FloatingActionButton>
-      <Paper style={imgStyle}></Paper>
-      <Paper style={imgStyle}></Paper>
-      <Paper style={imgStyle}></Paper>
-      <Paper style={imgStyle}></Paper>
-      </div>;
+
+      {this.state.images.map(o => <Paper 
+        key={o.id}
+        style={{
+          display: 'inline-block',
+          width: 70,
+          height: 100,
+          verticalAlign: 'middle',
+          margin: 10,
+          overflow: 'hidden',
+          position: 'relative',
+        }}>
+        <img src={o.url} 
+          style={{
+            position: 'absolute',
+            maxWidth: 70,
+            maxHeight: 100,
+          }}/>
+        {o.name}
+      </Paper>)}
+    </div>;
   }
 }
