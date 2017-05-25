899ee082969e6b5233541e708bda5aa72bec0997
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-05-25 04:45:25 +0200

    oprette felt til søgeprofil, #2326. Data herfra bruges dog endnu ikke, da vi på nuværende tidspunkt ikke bruger opensearch.

--- src/Main.js
+++ src/Main.js
@@ -9,6 +9,10 @@ import Results from './Results';
 import CoverOptions from './CoverOptions';
 import {generateCovers, renderPreviews} from './render';
 import DownloadSettings from './DownloadSettings';
+import TextField from 'material-ui/TextField';
+
+set('search.agency', localStorage.getItem('search.agency') || "715100")
+set('search.profile', localStorage.getItem('search.profile') || "ddb")
 
 export default class Main extends ReCom {
   constructor(props, context) {
@@ -50,13 +54,32 @@ export default class Main extends ReCom {
               />
             </Paper>
             <DownloadSettings />
-          </div>
+            <Paper style={{margin: 10, padding: 10}}>
+              <TextField
+                floatingLabelText="Agency"
+                value={this.get('search.agency')}
+                onChange={(_, val) => {
+                  localStorage.setItem('search.agency', val);
+                  this.set('search.agency', val)
+                }}
+              />
+              <TextField
+                floatingLabelText="Profile"
+                value={this.get('search.profile')}
+                onChange={(_, val) => {
+                  localStorage.setItem('search.profile', val);
+                  this.set('search.profile', val)
+                }
+                }
+              />
+          </Paper>
+        </div>
 
-          <Paper
-            style={{
-              flex: '1 1 auto',
-              margin: 10,
-              padding: 10
+        <Paper
+          style={{
+            flex: '1 1 auto',
+            margin: 10,
+            padding: 10
             }}>
             <CoverOptions currentImage={currentImage} />
           </Paper>
